d88964ee63d279625dc591e78400f4e46ba02c2a
KamronAggorURI <kamron_aggor@uri.edu>
2025-05-01 17:14:04 -0400

    Update MapFetcher.py

--- code/MapFetcher.py
+++ code/MapFetcher.py
@@ -1,4 +1,9 @@
-# ri_map_fetcher.py
+# MapFetcher.py
+# This file defines RI_MapFetcher class; 
+# functions to get town coordinates based on town name in RI, as well as the traffic incidents
+
+# class RI_MapFetcher -> init using key, then fetch loc + indicent info upon call
+
 
 import requests
 
@@ -6,7 +11,9 @@ class RI_MapFetcher:
     def __init__(self, api_key):
         self.api_key = api_key
 
+    @st.cache_data(ttl=900) # Kept crashing due to constant reloads, cache result for 15 mins
     def get_town_coords(self, town_name):
+        # Ref -> https://developer.mapquest.com/documentation/api/geocoding/address/get.html
         geocode_url = f'https://www.mapquestapi.com/geocoding/v1/address?key={self.api_key}&location={town_name},RI'
         response = requests.get(geocode_url)
         response.raise_for_status()
@@ -14,6 +21,7 @@ class RI_MapFetcher:
         loc = data['results'][0]['locations'][0]['latLng']
         return loc['lat'], loc['lng']
 
+    @st.cache_data(ttl=900)  # Kept crashing due to constant reloads, cache result for 15 mins
     def get_traffic_incidents(self, bounding_box='42,-71,41,-72'):
         traffic_url = f'https://www.mapquestapi.com/traffic/v2/incidents?key={self.api_key}&boundingBox={bounding_box}&filters=construction,incidents'
         response = requests.get(traffic_url)
