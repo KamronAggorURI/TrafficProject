5edd847482205f20853231506403b2aa9ddb0665
KamronAggorURI <kamron_aggor@uri.edu>
2025-05-04 00:32:27 -0400

    Update MapFetcher.py

--- code/MapFetcher.py
+++ code/MapFetcher.py
@@ -15,7 +15,7 @@ class RI_MapFetcher:
     @st.cache_data(ttl=900) # Kept crashing due to constant reloads, cache result for 15 mins
     def get_town_coords(_self, town_name):
         # Ref -> https://developer.mapquest.com/documentation/api/geocoding/address/get.html
-        geocode_url = f'https://www.mapquestapi.com/geocoding/v1/address?key={self.api_key}&location={town_name},RI'
+        geocode_url = f'https://www.mapquestapi.com/geocoding/v1/address?key={_self.api_key}&location={town_name},RI'
         response = requests.get(geocode_url)
         response.raise_for_status()
         data = response.json()
@@ -24,7 +24,7 @@ class RI_MapFetcher:
 
     @st.cache_data(ttl=900)  # Kept crashing due to constant reloads, cache result for 15 mins
     def get_traffic_incidents(_self, bounding_box='42,-71,41,-72'):
-        traffic_url = f'https://www.mapquestapi.com/traffic/v2/incidents?key={self.api_key}&boundingBox={bounding_box}&filters=construction,incidents'
+        traffic_url = f'https://www.mapquestapi.com/traffic/v2/incidents?key={_self.api_key}&boundingBox={bounding_box}&filters=construction,incidents'
         response = requests.get(traffic_url)
         response.raise_for_status()
         data = response.json()
