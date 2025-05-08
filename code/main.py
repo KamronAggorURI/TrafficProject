d261f3420ceaa083adcc62c3d6e2300378517f56
KamronAggorURI <kamron_aggor@uri.edu>
2025-05-07 21:38:17 -0400

    Update main.py

--- code/main.py
+++ code/main.py
@@ -8,15 +8,15 @@ import requests
 api_key = 'LhMzXgy2kWSqeCJZwc5XYClmjw9vvkIh'
 fetcher = RI_MapFetcher(api_key)
 
-# Example: Get Rhode Island state map
-state_map = fetcher.get_state_map()
-state_map.show()
+# Fetch RI state map
+# state_map = fetcher.get_state_map()
+# state_map.show()
 
-# Example: Get map of Providence
-prov_map = fetcher.get_town_map('Providence')
-prov_map.show()
+# Fetch map of Providence
+# prov_map = fetcher.get_town_map('Providence')
+# prov_map.show()
 
-# Example: Get traffic incidents (MapQuest traffic API)
+# Fetch traffic incidents (via MapQuest traffic API)
 traffic_url = f'https://www.mapquestapi.com/traffic/v2/incidents?key={api_key}&boundingBox=42,-71,41,-72&filters=construction,incidents'
 response = requests.get(traffic_url)
 response.raise_for_status()
